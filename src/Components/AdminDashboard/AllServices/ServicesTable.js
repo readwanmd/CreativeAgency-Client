3ee3e28189cd285ef1d088f3c3abda2af62b5c4a
readwanmd <readwanmd@gmail.com>
2020-10-16 17:39:52 +0600

    loading gif added

--- src/Components/AdminDashboard/AllServices/ServicesTable.js
+++ src/Components/AdminDashboard/AllServices/ServicesTable.js
@@ -1,30 +1,37 @@
 import React from 'react';
+import loading from '../../../images/loading.gif';
 
 const ServicesTable = ({serviceTableData}) => {
     return (
         <div>
-            <table class="table text-center mt-5" style={{backgroundColor: '#fff', width: '90%', margin: '0 auto'}}>
-                <thead class="thead-dark">
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Service</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                       serviceTableData.map(data => 
+            { (serviceTableData.length === 0) ? 
+                <div className="d-flex justify-content-center">
+                    <img className="mt-3" style={{width: '200px', height: '150px'}} src={loading} alt=""/> 
+                </div> :
+                
+                <table class="table text-center mt-5" style={{backgroundColor: '#fff', width: '90%', margin: '0 auto'}}>
+                    <thead class="thead-dark">
                         <tr>
-                            <td>{data.name}</td>
-                            <td>{data.email}</td>
-                            <td>{data.projectName}</td>
-                            <td>{data.status}</td>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Service</th>
+                            <th>Status</th>
                         </tr>
-                        ) 
-                    }
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {
+                        serviceTableData.map(data => 
+                            <tr>
+                                <td>{data.name}</td>
+                                <td>{data.email}</td>
+                                <td>{data.projectName}</td>
+                                <td>{data.status}</td>
+                            </tr>
+                            ) 
+                        }
+                    </tbody>
+                </table>
+            }
         </div>
     );
 };
